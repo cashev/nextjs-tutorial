bd40b42886270ac6da9389e3e5cd2408be1a053a
cashev <42924021+cashev@users.noreply.github.com>
2023-07-09 14:50:01 +0000

    fix Warning: Each child in a list should have a unique "key" prop.

--- components/organisms/carousel-products.tsx
+++ components/organisms/carousel-products.tsx
@@ -15,7 +15,7 @@ const CarouselProducts: React.FC<CarouselProductsProps> = ({ products }) => {
     <div className='relative w-full overflow-hidden bg-black dark:bg-white'>
       <div className='flex animate-carousel'>
         {[...products, ...products].map((product, index) => (
-          <Link href={product.url} className='relative h-[30vh] w-1/2 flex-none md:w-1/3'>
+          <Link key={index} href={product.url} className='relative h-[30vh] w-1/2 flex-none md:w-1/3'>
             <ImageLabelCenterRight key={index} imageUrl={product.image} altText={product.productName} label={product.productName} />
           </Link>
         ))}
