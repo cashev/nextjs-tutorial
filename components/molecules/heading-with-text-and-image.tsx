b53ec6005f6061747f7df9567b7b384836139f8c
cashev <42924021+cashev@users.noreply.github.com>
2023-07-09 00:57:18 +0000

    use next/image

--- components/molecules/heading-with-text-and-image.tsx
+++ components/molecules/heading-with-text-and-image.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from '../atoms/image';
+import Image from 'next/image';
 
 interface HeadingWithTextAndImageProps {
   heading: string;
@@ -11,7 +11,7 @@ interface HeadingWithTextAndImageProps {
 const HeadingWithTextAndImage: React.FC<HeadingWithTextAndImageProps> = ({ heading, text, imageUrl, altText }) => {
   return (
     <div className="relative flex h-full w-full items-center justify-center overflow-hidden relative">
-      <Image src={imageUrl} alt={altText} />
+      <Image src={imageUrl} alt={altText} width={1080} height={1080} />
       <div className='absolute left-0 top-0 w-3/4 text-black dark:text-white'>
         <h3 className='inline bg-white box-decoration-clone py-3 pl-5 font-semibold leading-loose shadow-[1.25rem_0_0] shadow-white dark:bg-black dark:shadow-black text-3xl'>{heading}</h3>
         <p className='w-fit bg-white px-5 py-3 text-sm font-semibold dark:bg-black dark:text-white'>{text}</p>
--- components/molecules/image-label-center-right.tsx
+++ components/molecules/image-label-center-right.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Image from '../atoms/image';
-import Label from '../atoms/label';
+import Image from 'next/image';
 
 interface ImageLabelCenterRightProps {
   imageUrl: string;
@@ -11,7 +10,7 @@ interface ImageLabelCenterRightProps {
 const ImageLabelCenterRight: React.FC<ImageLabelCenterRightProps> = ({ imageUrl, altText, label }) => {
   return (
     <div className='relative h-[30vh] w-1/2 flex-none md:w-1/3'>
-      <Image src={imageUrl} alt={altText} className='h-full object-contain' sizes='33vw' style={{ position: "absolute", height: "100%", width: "100%", left: "0", top: "0", right: "0", bottom: "0", color: "transparent" }} />
+      <Image src={imageUrl} alt={altText} className='h-full object-contain' sizes='33vw' width={1080} height={1080} style={{ position: "absolute", height: "100%", width: "100%", left: "0", top: "0", right: "0", bottom: "0", color: "transparent" }} />
       <div className='absolute inset-y-0 right-0 flex items-center justify-center'>
         <div className='inline-flex bg-white p-4 text-xl font-semibold text-black dark:bg-black dark:text-white'>
           {label}
